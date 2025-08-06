50f26a5e73f6314980ff159f16f78441e3495381
LuciaSaravia <luciaasaravia@gmail.com>
2025-08-06 12:52:58 -0300

    hola

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,5 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+class Botella:
+    def __init__(self):
+        nombre = 'Lucia Saravia'
