dbbfea35356a85de36eacb1e772f2a2e569050d2
MariaPazA <mariapaz.almiron@ingenieria.uner.edu.ar>
2025-10-02 12:58:17 -0300

    agrego el orden de los pacientes segun criticidad y orden de llegada

--- TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
+++ TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
@@ -8,9 +8,17 @@ class ColaPrioridad:
     def agregar_nuevo_paciente(self, paciente_nuevo):
         if not isinstance(paciente_nuevo, Paciente):
             raise ValueError('El paciente tiene que ser un numero entero')
+        paciente_nuevo._Paciente__tiempo = self.__contador_llegadas
+        self.__contador_llegadas += 1
         self.__monticulo.insertar(paciente_nuevo)
 
     def atender_paciente(self):
-        self.__monticulo.eliminarMin()
+        if self.__monticulo==0:
+            return None
+        
+        return self.__monticulo.eliminarMin()
+    
+    def hay_pacientes(self):
+        return self.__monticulo.tamanoActual > 0
 
     
