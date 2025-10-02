0c699aad91929ef3a8b9147eb5394051e14de335
MariaPazA <mariapaz.almiron@ingenieria.uner.edu.ar>
2025-10-02 13:04:23 -0300

    subo cambios

--- TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
+++ TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
@@ -3,17 +3,17 @@ from modules.paciente import Paciente
 class ColaPrioridad:
     def __init__(self):
         self.__monticulo = MonticuloMin()
-    
+        self.__contador_llegadas = 0
 
     def agregar_nuevo_paciente(self, paciente_nuevo):
         if not isinstance(paciente_nuevo, Paciente):
-            raise ValueError('El paciente tiene que ser un numero entero')
-        paciente_nuevo._Paciente__tiempo = self.__contador_llegadas
+            raise ValueError('El objeto debe ser tipo Paciente')
+        paciente_nuevo._Paciente__tiempo = self.__contador_llegadas #asignamos un orden de llegada
         self.__contador_llegadas += 1
         self.__monticulo.insertar(paciente_nuevo)
 
     def atender_paciente(self):
-        if self.__monticulo==0:
+        if self.__monticulo.tamanoActual==0:
             return None
         
         return self.__monticulo.eliminarMin()
