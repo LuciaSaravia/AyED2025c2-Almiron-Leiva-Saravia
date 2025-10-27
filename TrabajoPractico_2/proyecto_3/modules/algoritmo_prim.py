a6a8fa987c67429a4be8e7d96455acadd608f582
MariaPazA <mariapaz.almiron@ingenieria.uner.edu.ar>
2025-10-27 16:49:25 -0300

    subo cambios en la cola de prioridad y prim, las funciones las probe y andan

--- TrabajoPractico_2/proyecto_3/modules/algoritmo_prim.py
+++ TrabajoPractico_2/proyecto_3/modules/algoritmo_prim.py
@@ -1,4 +1,5 @@
 import sys
+from modules import grafo
 from modules.cola_prioridad import ColaPrioridad
 from modules.grafo import Grafo, Vertice
 
@@ -10,11 +11,12 @@ def prim(G: Grafo,inicio):
         v.distancia = sys.maxsize #el maxsize hace que sea hasta el infinito la distancia entre as aldeas
         v.predecesor = None
     inicio.distancia = 0
-    cp.construirMonticulo([(v.distancia,v) for v in G]) #se crea la cp con las distancias de cada vertice, es una tupla
-    
+    distancias = [(v.distancia, v) for v in G] 
+    cp.construirMonticulo(distancias)
+    #se crea la cp con las distancias de cada vertice, es una tupla
+
     while not cp.estaVacia():
-        verticeActual = cp.eliminarMin()[1] #al verticeActual se le asigan el minimo de la cp, guarda el indice por eso [1]
-        print(f'Procesando vertices {verticeActual.obtener()}')
+        verticeActual = cp.eliminarMin()[1] #al verticeActual se le asigan el minimo de la cp, guarda el indice por eso [1
         for verticeSiguiente in verticeActual.obtener_vecinos(): #recorre las conexiones del verticeActual
             nuevoCosto = verticeActual.obtener_ponderacion(verticeSiguiente) #ponderacion entre el actual y el siguiente
             print(f'evaluando vecinos con costo {nuevoCosto}')
@@ -22,8 +24,8 @@ def prim(G: Grafo,inicio):
                 #relajacion del algoritmo de prim y algortimo de dijkstra
                 verticeSiguiente.predecesor = verticeActual # se actualiza el predecesor que es el actual
                 verticeSiguiente.distancia = nuevoCosto #se ajusta la distancia del verticeSiguiente
-                cp.decrementar_clave(verticeSiguiente, nuevoCosto) #se decrementa la clave del siguiente vertice en la cp
-                print(f"    Actualizado: {verticeActual.obtenerId()} -> {verticeSiguiente.obtenerId()} con costo {nuevoCosto}")
+                cp.decrementarClave(verticeSiguiente, nuevoCosto) #se decrementa la clave del siguiente vertice en la cp
+                print(f"    Actualizado: {verticeActual.obtener()} -> {verticeSiguiente.obtener()} con costo {nuevoCosto}")
                 #hay que reconstruir para eso nos fijamos en los predecesores y calcular el costo minimo que es la suma de las distancias
 
 def distanciatotal(grafo: Grafo): # Suma las distancias del árbol de expansión mínima
@@ -32,29 +34,24 @@ def distanciatotal(grafo: Grafo): # Suma las distancias del árbol de expansión
         if v.predecesor is not None:
             total += v.distancia
     return total
-
+#
 if __name__=='__main__':
-    listaMonticulo = [('a', 1),('b', 5),('c', 6)]
-    inicio = Vertice ('a')
     g = Grafo ()
-    # g.agregarArista(3,5,1)
-    # g.agregarArista(5,3,1)
-    # g.agregarArista(3,7,6)
-    # g.agregarArista(7,3,6)
+    #ejemplo con numeros
+    g.agregarArista(3,5,1.0)
+    g.agregarArista(3,7,6.0)
+    g.agregarArista(5,7,2.0)
+    inicio=g.obtenerVertice(3)
+    prim(g, inicio)
+    print(f"Distancia total: {distanciatotal(g)}")
+    #ejemplo con letras y numeros
+    gl= Grafo ()
+    gl.agregarVertice('a')
+    gl.agregarVertice('b')
+    gl.agregarVertice('c')
+    gl.agregarArista('a', 'b', 2)
+    gl.agregarArista("b", "c", 5)
+    gl.agregarArista('a', 'c', 3)
 
-    # prim(g, g.obtenerVertice(3))
-    # print("Árbol de expansión mínima:")
-    # for v in g:
-    #     if v.predecesor is not None:
-    #         print(f"{v.predecesor.obtenerId()} - {v.obtenerId()} con costo {v.distancia}")
-
-    # print(f"La distancia total es: {distanciatotal(g)}")
-    # grafo.agregarVertice('a')
-    # grafo.agregarVertice('b')
-    # grafo.agregarVertice('c')
-    # grafo.agregarArista('a', 'b', 2)
-    # grafo.agregarArista("b", "c", 5)
-    # grafo.agregarArista('a', 'c', 3)
-    
-    # prim(grafo, inicio)
-    # print(distanciatotal(grafo))
+    prim(gl, gl.obtenerVertice('a'))
+    print(distanciatotal(gl))
--- TrabajoPractico_2/proyecto_3/modules/cola_prioridad.py
+++ TrabajoPractico_2/proyecto_3/modules/cola_prioridad.py
@@ -19,42 +19,54 @@ class ColaPrioridad:
     def tamanio(self):
         return self.tamanoActual
 
-    def construirMonticulo(self, distancias):
-        i = len(distancias) // 2
-        self.tamanoActual = len(distancias)
-        self.listaMonticulo = [None] + distancias[:]
+    def construirMonticulo(self, distancias): #distancias puede ser una lista de tuplas o una lista simple
+        #Poreso aca, si el item en distancias es una tupla, lo agrego tal cual, sino lo convierto en tupla (item, None)
+        lista_preparada = []
+        for item in distancias:
+            if isinstance(item, tuple):
+                lista_preparada.append(item)
+            else:
+                lista_preparada.append((item, None))
+                
+        self.tamanoActual = len(lista_preparada)
+        self.listaMonticulo = [None] + lista_preparada
+        i = len(lista_preparada) // 2
         while (i > 0):
             self.infiltAbajo(i)
             i = i - 1
 
     def eliminarMin(self):
         '''Devuelve la raiz del monticulo y selecciona una nueva raiz'''
+        if self.tamanoActual == 0:
+            return None
         valorSacado = self.listaMonticulo[1]
         self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
         self.tamanoActual = self.tamanoActual - 1
         self.listaMonticulo.pop()
-        self.infiltAbajo(1)
+        if self.tamanoActual > 0:
+            self.infiltAbajo(1)
         return valorSacado
 
     def infiltArriba(self, valor):
-            while valor // 2 > 0:
-                if self.listaMonticulo[valor][0] < self.listaMonticulo[valor // 2][0]:
-                    tmp = self.listaMonticulo[valor // 2]
-                    self.listaMonticulo[valor // 2] = self.listaMonticulo[valor]
-                    self.listaMonticulo[valor] = tmp
-                elif self.listaMonticulo[valor][0] < self.listaMonticulo[(valor //2) + 1][0]:
-                    tmp = self.listaMonticulo[(valor//2)+1]
-                    self.listaMonticulo[(valor//2)+1] = self.listaMonticulo[valor]
-                    self.listaMonticulo[valor] = tmp
+        while valor // 2 > 0:
+            if self.listaMonticulo[valor][0] < self.listaMonticulo[valor // 2][0]:
+                tmp = self.listaMonticulo[valor // 2]
+                self.listaMonticulo[valor // 2] = self.listaMonticulo[valor]
+                self.listaMonticulo[valor] = tmp
+            elif self.listaMonticulo[valor][0] < self.listaMonticulo[(valor //2) + 1][0]:
+                tmp = self.listaMonticulo[(valor//2)+1]
+                self.listaMonticulo[(valor//2)+1] = self.listaMonticulo[valor]
+                self.listaMonticulo[valor] = tmp
             valor = valor // 2
 
     def infiltAbajo(self,i):
             while (i * 2) <= self.tamanoActual:
                 hm = self.hijoMin(i)
-                if self.listaMonticulo[i][0] > self.listaMonticulo[hm][0]:
-                    tmp = self.listaMonticulo[i]
-                    self.listaMonticulo[i] = self.listaMonticulo[hm]
-                    self.listaMonticulo[hm] = tmp
+                if self.listaMonticulo[i] is not None and self.listaMonticulo[hm] is not None:
+                    if self.listaMonticulo[i][0] > self.listaMonticulo[hm][0]: 
+                        tmp = self.listaMonticulo[i]
+                        self.listaMonticulo[i] = self.listaMonticulo[hm]
+                        self.listaMonticulo[hm] = tmp
                 i = hm
 
     def hijoMin(self,i):
@@ -71,11 +83,19 @@ class ColaPrioridad:
             return
         vertice.distancia = nueva_ponderacion
         for i in range(1, self.tamanoActual + 1):
-            dist, v = self.listaMonticuloMonticulo[i]
+            dist, v = self.listaMonticulo[i]
             if v is vertice:
-                self.listaMonticuloMonticulo[i] = (nueva_ponderacion, v)
+                self.listaMonticulo[i] = (nueva_ponderacion, v)
                 self.infiltArriba(i)
                 break
+            
+    def __contains__(self, vertice: Vertice):
+        for i in range(1, self.tamanoActual + 1):
+            dist, v = self.listaMonticulo[i]
+            if v is vertice:
+                return True
+        return False
+    
     def __iter__(self):
         return iter(self.listaMonticulo[1:]) 
     
@@ -83,6 +103,20 @@ if __name__=='__main__':
     listaMonticulo = [1, 5, 6, 14, 9, 27, 31, 2]
     cp = ColaPrioridad()
     cp.construirMonticulo(listaMonticulo)
-    print(cp.tamanoActual)
-    print(cp.eliminarMin())
-    print(cp.listaMonticulo)
+    print("tamaño actual", cp.tamanoActual)
+    print("mínimo eliminado", cp.eliminarMin())
+    print("estado de la lista de montículo", cp.listaMonticulo)   
+    
+    # Prueba con vértices
+    from modules.grafo import Grafo
+    g = Grafo()
+    g.agregarArista(1, 2, 1)
+    g.agregarArista(2, 3, 2)
+    g.agregarArista(3, 4, 3)
+    g.agregarArista(4, 1, 4)
+    g.agregarArista(5, 3, 5)
+    vertices = [(float('inf'), v) for v in g]
+    cp2 = ColaPrioridad()
+    cp2.construirMonticulo(vertices)
+    print("\nPrueba con vértices")
+    print("tamaño actual", cp2.tamanoActual)
